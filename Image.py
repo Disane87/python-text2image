6a88c26662e1caa7a4a452b815ab0894899f1166
Marco <disane@icloud.com>
2018-09-25 17:45:56 +0200

    Wordpress auto upload activated & config save to json

--- Image.py
+++ Image.py
@@ -1,92 +1,114 @@
+# Needs min Python 3
+# 
+
 from PIL import Image, ImageDraw, ImageFont
 import os
 import re
+import requests
+import json
+import base64
+from settings import Settings
 
-# settings
-source_image = "images/pexels-photo-442573.jpeg"
-txt = "Text automatisiert in Bilder"+os.linesep+"einfügen mit Python"
-txt_color = "white"
-fontsize = 1
-fontfile = "font/LuckiestGuy.ttf"
-border = True
-border_color = "black" 
-border_size = 5
-output_dir = "output"
-text_alignment = "center"
+def main():    
+    # settings
+    source_image = "images/pexels-photo-442573.jpeg"
+    txt = "Text automatisiert in Bilder"+os.linesep+"einfügen mit Python"
+    settings = Settings("config.json")
 
-# portion of image width you want text width to be
-img_fraction = 1
+    # internal settings, no need to change them!
+    fontsize = 1
 
-# line height and width initial value
-line_height = 0
-line_width = 0
+    # generating images
+    image = Image.open(source_image).convert('RGBA')
+    image_with_text = Image.new('RGBA', image.size, (255,255,255,0))
+    draw = ImageDraw.Draw(image_with_text)
 
-# generating images
-image = Image.open(source_image).convert('RGBA')
-image_with_text = Image.new('RGBA', image.size, (255,255,255,0))
-draw = ImageDraw.Draw(image_with_text)
+    # initial position of the text
+    font_pos_x = 0
+    font_pos_y = 0
 
-# initial position of the text
-font_pos_x = 0
-font_pos_y = 0
+    # getting font
+    font = ImageFont.truetype(settings.fontfile, fontsize)
 
-# getting font
-font = ImageFont.truetype(fontfile, fontsize)
+    # caluclate position of text (centered)
+    image_width = image.size[0]
+    image_height = image.size[1]
 
-# caluclate position of text (centered)
-image_width = image.size[0]
-image_height = image.size[1]
-print("Image height: "+str(image_height))
-print("Image width: "+str(image_width))
+    # line height and width initial value
+    line_height = 0
+    line_width = 0
 
-# getting the perfect size of the fraction of teh text to fit in the image
-while (font.getsize(txt)[0] < img_fraction*image.size[0]):
-    fontsize += 1
-    font = ImageFont.truetype(fontfile, fontsize)
+    print("Image height: "+str(image_height))
+    print("Image width: "+str(image_width))
 
- # check if there is a new line terminator
-if txt.find(os.linesep) >= 0:
-    print("New line found. Checking longest line width")
-    lines = txt.split(os.linesep)
-    for line in lines:
+    # getting the perfect size of the fraction of teh text to fit in the image
+    while (font.getsize(txt)[0] < settings.img_fraction*image.size[0]):
+        fontsize += 1
+        font = ImageFont.truetype(settings.fontfile, fontsize)
 
-        # find the longest line
-        if(font.getsize(line)[0] > line_width):
-            line_width = font.getsize(line)[0]
+    # check if there is a new line terminator
+    if txt.find(os.linesep) >= 0:
+        print("New line found. Checking longest line width")
+        lines = txt.split(os.linesep)
+        for line in lines:
 
-        line_height += font.getsize(line)[1]
-else:    
-    line_width = font.getsize(txt)[0]
-    line_height = font.getsize(txt)[1]
+            # find the longest line
+            if(font.getsize(line)[0] > line_width):
+                line_width = font.getsize(line)[0]
+
+            line_height += font.getsize(line)[1]
+    else:    
+        line_width = font.getsize(txt)[0]
+        line_height = font.getsize(txt)[1]
 
 
-# calculating text position
-font_pos_x = (image_width - line_width) / 2
-font_pos_y = (image_height - line_height) / 2 
+    # calculating text position
+    font_pos_x = (image_width - line_width) / 2
+    font_pos_y = (image_height - line_height) / 2 
 
-# optionally de-increment to be sure it is less than criteria
-fontsize -= 1
-font = ImageFont.truetype(fontfile, fontsize)
+    # optionally de-increment to be sure it is less than criteria
+    fontsize -= 1
+    font = ImageFont.truetype(settings.fontfile, fontsize)
 
-print("Final font size: ",fontsize)
+    print("Final font size: ",fontsize)
 
-# applying border if needed
-if(border):
-    print("Applying border ",fontsize)
-    draw.text((font_pos_x-border_size, font_pos_y-border_size), txt, font=font, fill=border_color, align=text_alignment)
-    draw.text((font_pos_x+border_size, font_pos_y-border_size), txt, font=font, fill=border_color, align=text_alignment)
-    draw.text((font_pos_x-border_size, font_pos_y+border_size), txt, font=font, fill=border_color, align=text_alignment)
-    draw.text((font_pos_x+border_size, font_pos_y+border_size), txt, font=font, fill=border_color, align=text_alignment)
+    # applying border if needed
+    if(settings.border):
+        print("Applying border ",fontsize)
+        draw.text((font_pos_x-settings.border_size, font_pos_y-settings.border_size), txt, font=font, fill=settings.border_color, align=settings.text_alignment)
+        draw.text((font_pos_x+settings.border_size, font_pos_y-settings.border_size), txt, font=font, fill=settings.border_color, align=settings.text_alignment)
+        draw.text((font_pos_x-settings.border_size, font_pos_y+settings.border_size), txt, font=font, fill=settings.border_color, align=settings.text_alignment)
+        draw.text((font_pos_x+settings.border_size, font_pos_y+settings.border_size), txt, font=font, fill=settings.border_color, align=settings.text_alignment)
 
-# draw text to image
-draw.text((font_pos_x, font_pos_y), txt, font=font, fill=txt_color, align=text_alignment) # put the text on the image
+    # draw text to image
+    draw.text((font_pos_x, font_pos_y), txt, font=font, fill=settings.txt_color, align=settings.text_alignment) # put the text on the image
 
-# overlay text layer with the picture
-out = Image.alpha_composite(image, image_with_text)
+    # overlay text layer with the picture
+    out = Image.alpha_composite(image, image_with_text)
 
-# show image
-out.show()
+    # show image
+    out.show()
 
-# save image
-file_name = re.sub(r"[^a-zA-Z0-9 | ]*","", txt).replace(" ", "_")+'.png'
-out.save(os.path.join(output_dir, file_name)) # save it
+    # save image if not in previewmode
+    if settings.preview_mode:
+        file_name = re.sub(r"[^a-zA-Z0-9 | ]*","", txt).replace(" ", "_")+'.png'
+        out.save(os.path.join(settings.output_dir, file_name)) # save it
+
+        if settings.wp_auto_upload:
+            token = base64.standard_b64encode((settings.wp_user + ':' + settings.wp_auth_key).encode('utf-8')) # we have to encode the usr and pw
+            headers = {'Authorization': 'Basic ' + token.decode('utf-8')}
+            media = {'file': open(os.path.join(settings.output_dir, file_name),'rb')} # 'picture.jpg' path to the image
+            data = {'description': txt, 'title': txt}
+
+            image = requests.post(settings.wp_url + '/media', headers=headers, files=media, data=data)
+            link = json.loads(image.content.decode('utf-8'))['link']
+            postid =json.loads(image.content.decode('utf-8'))['id']
+
+            print('Your image is published on {} with ID {}'.format(link, postid))
+
+            if settings.debug:
+                resp = requests.delete(settings.wp_url + '/media/'+str(postid))
+                print('DEBUG: Media with ID {} deleted.'.format(postid))
+
+main()
+
---
+++ settings.py
@@ -0,0 +1,73 @@
+import os 
+import json
+from json import JSONEncoder
+
+class Settings:
+
+    txt_color = ""
+    fontfile = ""
+    border = True
+    border_color = "" 
+    border_size = 0
+    output_dir = ""
+    text_alignment = ""
+    preview_mode = True
+
+    # debug mode 
+    debug = True
+
+    # wordpress settings
+    wp_auto_upload = True # only valid if preview_mode is true
+    wp_user = ""
+    wp_url = ""
+    wp_auth_key = ""
+    # portion of image width you want text width to be
+    img_fraction = 1
+
+    _settings = None
+
+    def __init__(self, settings_file):
+
+        self.txt_color = "white"
+        self.fontfile = "font/LuckiestGuy.ttf"
+        self.border = True
+        self.border_color = "black" 
+        self.border_size = 5
+        self.output_dir = "output"
+        self.text_alignment = "center"
+        self.preview_mode = True
+
+        # debug mode 
+        self.debug = True
+
+        # wordpress settings
+        self.wp_auto_upload = True # only valid if preview_mode is true
+        self.wp_user = "Disane"
+        self.wp_url = "https://[YOUR URL]/wp-json/wp/v2"
+        self.wp_auth_key = "[YOUR API KEY]"
+        # portion of image width you want text width to be
+        img_fraction = 1
+
+        
+        if(os.path.getsize(settings_file) > 0):
+            sets = json.loads(open(settings_file, 'r', encoding='utf-8').read())
+            for key, value in sets.items():
+                self[key] = value
+        else:
+            with open(settings_file, 'w+') as outfile:
+                print("Class encoded: "+ MyEncoder().encode(self))
+                json.dump(self, outfile, cls=MyEncoder) #cls=MyEncoder
+
+    def getSetting(name):
+        return _setting[name]
+
+    def __getitem__(self, key):
+        return self[key]
+    
+    def __setitem__ (self, key, value):
+        if key in self:
+            self[key] = value
+
+class MyEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__ 
